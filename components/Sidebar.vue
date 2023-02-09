60afa146ff13766f8400299546ea7ba3cdf1f4bd
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-02-09 17:30:05 -0500

    Update Sidebar.vue

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -65,7 +65,7 @@
       </nuxt-link>
        <nuxt-link to="https://www.iftiinhub.pro/homepage">
         <div class="bg-elevation-2-hover transition-button rounded-md p-2">
-          <Cog6ToothIcon class="h-7 w-7" />
+          <ArrowUturnLeftIcon class="h-7 w-7" />
         </div>
       </nuxt-link>
     </section>
